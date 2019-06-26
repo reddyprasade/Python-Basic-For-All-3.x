31929575cdcd610a674dbf6b6d8627af2e4e746b
REDDY PRASADE <15084222+reddyprasade@users.noreply.github.com>
2019-06-26 19:48:46 +0530

    While

---
+++ Whileloop.py
@@ -0,0 +1,58 @@
+#n = 10
+## initialize sum and counter
+#sum = 0
+#i = 1
+#while i <= n:
+#    sum = sum + i
+#    i = i+1    # update counter
+#
+## print the sum
+#print("The sum is", sum)
+#
+
+
+
+
+#count= 0
+#while count<3:
+#    print("Inside a while loop")
+#    count+=1 #==> count=count+1
+#else:
+#    print("Inside Else")
+
+
+#for val in "srting":
+#    if val == "i":
+#        continue
+#    print(val)
+#print("The end")
+
+
+
+
+
+
+
+
+
+
+
+
+#s = {'p','y','t','h','o','n'}
+#for val in s:
+#    passni
+
+#while True:
+#    num=int(input("Enter the integer:"))
+#    print("The double of",num," is number 2" * num)
+vowel= "aeiouAEIOU"
+'''while True:
+    v = input("Enter Vowel:")
+    if v in vowel:
+        print("Give Char is Vowel",v)
+        break
+    print("this is not a vowel")
+print("Thank you")
+
+'''
+
