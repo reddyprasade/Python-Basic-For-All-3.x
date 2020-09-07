d1fc761ac7e5f4aa564cf8c078352027736d5ad7
REDDY PRASAD <15084222+reddyprasade@users.noreply.github.com>
2020-09-07 17:49:48 +0530

    Add files via upload

---
+++ Variables/Variables.py
@@ -0,0 +1,60 @@
+# Editor
+print("Hi")
+
+# we Are Syntax Error
+# Print("Hello")
+print("Hello")
+
+
+# Difference Between Shell and Editor
+
+# Shell : 1. We are Write a Code but we can't modify
+#         2. We can't Save the code in Shell
+#         3. Shell Consist of Debug,Shell
+
+
+# Editor: 1. We can write a Code and We save the code and we can Modify the code
+        # 2. Editor as Run the code Many times
+        # 3. In Editor we can Share and Rerun when you Need
+ 
+class Home:
+    pass
+
+
+
+# Variables
+# Def:  Variables are used to Store the data in it
+Var = 20 # hear `Var` is  Variable name and `=` is as assignment operator and 20 is a value
+print("My Varable values is:",Var)
+
+
+# Rules to Def a varables:
+"""
+1. we can't Create `Funcation names(print,input)` as `variable name`
+2. We can't Create keywords(def,class) as a Variable names
+3. We Can't use Number(0-9) and Special Charaters(@,#,$,%,&) as a Varable name
+
+
+
+1. We Can Any Cha from A-Z or a-z
+2. we Can Use _ as a varable name as with any number ,keyword,funcation
+"""
+# def = "hi"
+A = 10.25
+print("Alph Char as a varable:",A)
+
+a = 2j+3
+print("Alpha Cha as a varable Small",a)
+
+print(40*'*')
+
+_9 = "hi"
+print("Underscode with Number as a Varable name:",_9)
+
+_def = "Hello worlds"
+print("Underscode with keywords as a Varable name:",_def)
+
+_print="Python"
+print("Underscode with funcation as a Varable name:",_print)
+
+
