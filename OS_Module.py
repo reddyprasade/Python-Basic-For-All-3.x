54c5db6bccdbbe106c1ad498bbd9a76f7ccf564d
REDDY PRASAD <15084222+reddyprasade@users.noreply.github.com>
2021-03-15 14:41:40 +0530

    Create OS_Module.py

---
+++ OS_Module.py
@@ -0,0 +1,12 @@
+#### OS Module
+#Using python os module it is possible to automatically perform many operating system tasks. The OS module in Python provides functions for creating, changing current working directory, and removing a directory (folder), fetching its contents, changing and identifying the current directory.
+# import the module
+import os
+# Creating a directory
+os.mkdir('directory_name')
+# Changing the current directory
+os.chdir('path')
+# Getting current working directory
+os.getcwd()
+# Removing directory
+os.rmdir()
