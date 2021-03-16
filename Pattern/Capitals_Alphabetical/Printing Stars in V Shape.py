6bb8053ee56d433e8981abdc87fac1f6f3ec8857
REDDY PRASAD <15084222+reddyprasade@users.noreply.github.com>
2021-03-16 10:43:38 +0530

    Update Printing Stars in V Shape.py

--- Pattern/Capitals_Alphabetical/Printing Stars in V Shape.py
+++ Pattern/Capitals_Alphabetical/Printing Stars in V Shape.py
@@ -5,3 +5,19 @@ for row in range(4):
         else:
             print(' ',end=' ')
     print()
+
+    
+  ## Method -2 
+i =0 
+j = 6
+for row in range(4):
+    for col in range(7):
+        if row==col:
+            print('*',end=' ')
+        elif row==i and col==j:
+            print('*',end=' ')
+            i=i+1
+            j=j-1
+        else:
+            print(' ',end=' ')
+     print()
