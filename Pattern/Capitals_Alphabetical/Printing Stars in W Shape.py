aa6f8e2e392438392241aeb04aca1b35cbf72892
REDDY PRASAD <15084222+reddyprasade@users.noreply.github.com>
2021-03-16 10:54:33 +0530

    Update Printing Stars in W Shape.py

--- Pattern/Capitals_Alphabetical/Printing Stars in W Shape.py
+++ Pattern/Capitals_Alphabetical/Printing Stars in W Shape.py
@@ -6,3 +6,20 @@ for row in range(6):
         else:
             print(' ',end=' ')
     print()
+
+    
+# Method- 2 
+
+i = 0
+j = 3
+for row in range(4):
+    for col in range(7):
+        if col==0 or col==6 or (col==5 and row==2)or(col==4 and row==1):
+            print('*',end=' ')
+        elif row==i and col==j:
+            print('*',end=' ')
+            i+=1
+            j-=1
+        else:
+            print(' ',end=' ')
+    print()
