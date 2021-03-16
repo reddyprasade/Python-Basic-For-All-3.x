5ed0ddd144ca460a6b6dfae93109c0ebb7d2c656
REDDY PRASAD <15084222+reddyprasade@users.noreply.github.com>
2021-03-16 10:33:56 +0530

    Create Printing Stars in X Shape.py

---
+++ Pattern/Capitals_Alphabetical/Printing Stars in X Shape.py
@@ -0,0 +1,9 @@
+result_str="";    
+for row in range(0,7):    
+    for column in range(0,7):     
+        if (((column == 1 or column == 5) and (row > 4 or row < 2)) or row == column and column > 0 and column < 6 or (column == 2 and row == 4) or (column == 4 and row == 2)):  
+            result_str=result_str+"*"    
+        else:      
+            result_str=result_str+" "    
+    result_str=result_str+"\n"    
+print(result_str);
