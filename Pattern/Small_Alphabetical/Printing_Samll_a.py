b879a46cb55e4c355ec3cc7952e1c6e2b62a2bd5
REDDY PRASAD <15084222+reddyprasade@users.noreply.github.com>
2021-03-17 14:10:41 +0530

    Create Printing_Samll_a.py

---
+++ Pattern/Small_Alphabetical/Printing_Samll_a.py
@@ -0,0 +1,7 @@
+for row in range(7):
+		for col in range(5):
+			if row%3==0 and col>0 and col<4 or col==0 and row in (1,4,5) or col==4 and row>0 :
+				print('*', end = ' ')
+			else:
+				print(' ', end = ' ')
+		print()
