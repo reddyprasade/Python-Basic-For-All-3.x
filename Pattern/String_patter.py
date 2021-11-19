c490e19706d2138f9d58dbd5d728f4bb620d44cf
REDDY PRASAD <15084222+reddyprasade@users.noreply.github.com>
2021-11-19 10:46:37 +0530

    Update String_patter.py

--- Pattern/String_patter.py
+++ Pattern/String_patter.py
@@ -18,3 +18,43 @@ def print_formatted(number):
         print("{0:{width}d} {0:{width}o} {0:{width}X} {0:{width}b}".format(i, width=width))
 n = int(input())
 print_formatted(n)
+
+
+
+# Method -2 
+from future import print_function
+st=int(raw_input())
+w=len(bin(st)[2:])
+for i in range(1,st+1):
+    print((str(i).rjust(w,' '),str(oct(i)[1:]).rjust(w,' '),str(hex(i)[2:].upper()).rjust(w,' '),str(bin(i)[2:]).rjust(w,' '),sep=' '))
+    
+    
+    
+# Method-3 
+STDIN = int(raw_input())
+w = len(str(bin(STDIN)).replace('0b',''))
+
+for i in xrange(1, STDIN+1):
+    b = bin(int(i)).replace('0b','').rjust(w, ' ')
+    o = oct(int(i)).replace('0','', 1).rjust(w, ' ')
+    h = hex(int(i)).replace('0x','').upper().rjust(w, ' ')
+    j = str(i).rjust(w, ' ')
+    print(j, o, h, b)
+
+ # Method-4
+n = int(input())
+
+results = []
+
+for i in range(1, n+1):
+    decimal = str(i)
+    octal = str(oct(i)[2:])
+    hex_ = str(hex(i)[2:]).upper()
+    binary = str(bin(i)[2:])
+
+    results.append([decimal, octal, hex_, binary])
+
+width = len(results[-1][3])  # largest binary number
+
+for i in results:
+    print(*(rep.rjust(width) for rep in i))
