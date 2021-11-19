1f2b22c5b6ce40ba794a5a8f41c9ef3c188a73ea
REDDY PRASAD <15084222+reddyprasade@users.noreply.github.com>
2021-11-19 10:34:41 +0530

    Update Door_Mat.py

--- Pattern/Door_Mat.py
+++ Pattern/Door_Mat.py
@@ -1,4 +1,4 @@
-#def door_mat(N,M):
+# Method-1
 N, M = map(int,input().split())
 for i in range(1,N,2):
     print((i * ".|.").center(M, "-"))
@@ -7,4 +7,68 @@ print("WELCOME".center(M,"-"))
 for i in range(N-2,-1,-2): 
     print((i * ".|.").center(M, "-"))
 
-#door_mat(9,27)
+    
+# Method-2 
+n, m = map(int,input().split())
+pattern = [('.|.'*(2*i + 1)).center(m, '-') for i in range(n//2)]
+print('\n'.join(pattern + ['WELCOME'.center(m, '-')] + pattern[::-1]))
+
+# Method-3
+N, M = map(int, input().split(" "))
+    for i in range(N):
+        pattern = ".|."
+            if i < (N-1)/2:
+                print((pattern * (2*i+1)).center(M, "-"))
+            elif i == (N-1)/2:
+                print("WELCOME".center(M, "-"))
+            else:
+                print((pattern * (2*(N-1-i)+1)).center(M, "-"))
+# Method -4 
+def mat(x,y):
+    if x>5 and x<101 and y>15 and y<303:
+
+# Printing of the upper design before WELCOME
+        #for i in range(x//2):
+        q=1
+        for j in range(y//2-1,0,-3): #printing of triangle
+            print("-"*j,end="")
+            print(".|."*q,end="") 
+            q=q+2
+            print("-"*j)
+                
+                
+
+        print("-"*((y-7)//2),end="")
+        print("WELCOME",end="")
+        print("-"*((y-7)//2)) #Print the middle line first
+
+
+
+#Printing of the lower design after WELCOME
+        w=x-2
+        for k in range(3,y//2,3):
+            print("-"*k, end="")
+            print(".|."*w , end="")
+            w=w-2
+            print("-"*k)
+        
+
+
+x, y = [int(x) for x in input().split()] 
+mat(x,y)
+
+
+# Method-5 
+N,M= map(int,input().split())
+display_string="WELCOME"
+
+#upper half
+for j in range(1,N,2):
+    print("-"*int((M-(j*3))/2)+".|."*j+"-"*int((M-(j*3))/2))
+    
+#center
+print("-"*int((M-len(display_string))/2)+display_string+"-"*int((M-len(display_string))/2))
+
+#lower half
+for j in range(N-2,0,-2):
+    print("-"*int((M-(j*3))/2)+".|."*j+"-"*int((M-(j*3))/2))
